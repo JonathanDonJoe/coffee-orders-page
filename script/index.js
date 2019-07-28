a348b0a2256646fc2b16c2ea9cd9f6c6a7e2923b
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-07-27 21:49:46 -0400

    created basic structure of order details container.  Added comments for previously created functions.

--- script/index.js
+++ script/index.js
@@ -5,29 +5,36 @@
 // 5. add links with filtering by the first letter of the email (or coffee order, your choice)
 // that’s all for now, thanks y’all
 
-const orderContainer = document.querySelector('#order-container')
+const orderContainer = document.querySelector('#order-container');
+const orderDetailsContainer = document.querySelector('#order-details');
 
 drinksURL = 'http://my-little-cors-proxy.herokuapp.com/https://meanmom.jonathan-ray.com/drinks';
 ordersURL = 'http://my-little-cors-proxy.herokuapp.com/https://meanmom.jonathan-ray.com/orders';
-customersURL = 'http://my-little-cors-proxy.herokuapp.com/https://meanmom.jonathan-ray.com/customers'
+customersURL = 'http://my-little-cors-proxy.herokuapp.com/https://meanmom.jonathan-ray.com/customers';
 
 
+// Restructured orders to be {order.id:object} format
 function ordersById(jsonOrders) {
     const orderIdObject = {};
     jsonOrders.results.forEach(order => orderIdObject[order.id] = order)
     return orderIdObject;
 }
+
+// Restructured customers to be {customer.id:object} format
 function customerById(jsonCustomers) {
     const customerIdObject = {};
     jsonCustomers.results.forEach(cust => customerIdObject[cust.id]=cust);
     return customerIdObject;
 }
+
+// Restructured drinks to be {drink.id:object} format
 function drinksById(jsonDrinks) {
     const drinkIdObject = {};
     jsonDrinks.results.forEach(drink => drinkIdObject[cust.id]=drink);
     return drinkIdObject;
 }
 
+// Build Array of all current order id's
 function buildOrdersArray(orders) {
     const orderArray=[];
     for (order in orders){
@@ -36,10 +43,9 @@ function buildOrdersArray(orders) {
     return orderArray
 }
 
+// Populate Orders List Container with orders
 function fillOrdersContainer(orders, ordersById, customersById) {
-    // console.log(customersById['1'].email)
     orders.forEach(order => {
-        // console.log(ordersById[order].customer_id)
         const li = document.createElement('li');
         li.classList.add('order');
         li.innerHTML = `Order: ${order} <br>${customersById[ordersById[order].customer_id].email}`;
@@ -47,8 +53,27 @@ function fillOrdersContainer(orders, ordersById, customersById) {
     })
 }
 
+// Populate the order details container
+function populateOrderDetails(event,orders, drinks, customers) {
+    // Delete any old order elements
+    orderDetailsContainer.innerHTML = "";
+
+    // Create a containing div for the order
+    const section = document.createElement('section');
+    section.classList.add('test');  // Remove later.  For visuals
+    orderDetailsContainer.appendChild(section);
+
+    // Create Order Header
+    const orderID = event.target.innerHTML.split(' ')[1];
+    const h1 = document.createElement('h1');
+    h1.classList.add('order-details-h1')
+    h1.innerHTML = `Order: ${orderID}`;
+    section.appendChild(h1);
+}
+
 
 async function fetchMyData() {
+    // fetch and change to usable objects
     const fetchedDrinks = await fetch(drinksURL);
     const jsonifiedDrinks = await fetchedDrinks.json();
     const fetchedOrders = await fetch(ordersURL);
@@ -56,15 +81,18 @@ async function fetchMyData() {
     const fetchedCustomers = await fetch(customersURL);
     const jsonifiedCustomers = await fetchedCustomers.json();
     // console.log(jsonifiedDrinks);
+
+    // Restructured objects to be {id:object} format
     const ordersByIdObject = ordersById(jsonifiedOrders);
-    // console.log(ordersByIdObject)
     const drinksByIdObject = ordersById(jsonifiedDrinks);
-    // console.log(drinksByIdObject)
     const customersByIdObject = customerById(jsonifiedCustomers);
+
+    // Populate Orders Container sidebar
     const OrdersArray = buildOrdersArray(ordersByIdObject)
-    // console.log(OrdersArray)
     fillOrdersContainer(OrdersArray, ordersByIdObject, customersByIdObject);
 
+    // Add event listener for clicking on orders
+    orderContainer.addEventListener('click', event => populateOrderDetails(event, ordersByIdObject, drinksByIdObject, customersByIdObject))
 
     
 }
